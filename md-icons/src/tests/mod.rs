52a263159a6a64eb5db9cc697af942c5ac1a9924
Fionn Langhans <fionn.langhans@gmail.com>
2023-08-13 11:20:08 +0200

    Ran formatter

--- md-icons/src/tests/mod.rs
+++ md-icons/src/tests/mod.rs
@@ -18,9 +18,8 @@ mod filled {
     fn test_leptos_line_style() {
         let md = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\"><path d=\"M3 16h5v-2H3v2zm6.5 0h5v-2h-5v2zm6.5 0h5v-2h-5v2zM3 20h2v-2H3v2zm4 0h2v-2H7v2zm4 0h2v-2h-2v2zm4 0h2v-2h-2v2zm4 0h2v-2h-2v2zM3 12h8v-2H3v2zm10 0h8v-2h-8v2zM3 4v4h18V4H3z\"/></svg>";
 
-        let result = leptos::ssr::render_to_string(
-            |cx| leptos::view! { cx, <LeptosIconLineStyle /> }
-        );
+        let result =
+            leptos::ssr::render_to_string(|cx| leptos::view! { cx, <LeptosIconLineStyle /> });
 
         assert!(result.contains(md));
     }
